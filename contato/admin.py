6cfa017e775ea18e4e15b5a84d5c97a676111f5a
Sander Toledo <sanderaugusto@gmail.com>
2023-10-29 12:14:18 -0300

    Conf da tela Admin

--- contato/admin.py
+++ contato/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from contato import models
 
-# Register your models here.
+
+@admin.register(models.Contato)
+class Contato(admin.ModelAdmin):
+    list_display = "nome", "sobrenome", "telefone"
+    list_per_page = 10
+    list_max_show_all = 200
--- contato/models.py
+++ contato/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Contato(models.Model):
+    nome = models.CharField(max_length=50)
+    sobrenome = models.CharField(max_length=50, blank=True)
+    telefone = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254, blank=True)
+    criado = models.DateTimeField(default=timezone.now)
+    desc = models.TextField(blank=True)
+    
+    def __str__(self) -> str:
+        return f'{self.nome} {self.sobrenome}'
