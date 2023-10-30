9fbb2e81576cfbf28a87aeed3e2c531f1d980f74
Sander Toledo <sanderaugusto@gmail.com>
2023-10-30 14:40:40 -0300

    Criação do Model Categoria

--- contato/admin.py
+++ contato/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from contato import models
 
 
+@admin.register(models.Categoria)
+class Categoria(admin.ModelAdmin):
+    list_display = "nome"
+
+
 @admin.register(models.Contato)
 class Contato(admin.ModelAdmin):
     list_display = "nome", "sobrenome", "telefone"
--- contato/models.py
+++ contato/models.py
@@ -2,6 +2,13 @@ from django.db import models
 from django.utils import timezone
 
 
+class Categoria(models.Model):
+    nome = models.CharField(max_length=50)
+
+    def __str__(self)->str:
+        return f"{self.nome}"
+
+
 class Contato(models.Model):
     nome = models.CharField(max_length=50)
     sobrenome = models.CharField(max_length=50, blank=True)
@@ -11,6 +18,12 @@ class Contato(models.Model):
     desc = models.TextField(blank=True)
     mostrar = models.BooleanField(default=False)
     imagem = models.ImageField(blank=True, upload_to="imagens/%Y/%m/")
+    categoria = models.ForeignKey(
+        Categoria, 
+        on_delete=models.SET_NULL, 
+        blank=True, 
+        null=True
+    )
 
     def __str__(self) -> str:
         return f"{self.nome} {self.sobrenome}"
