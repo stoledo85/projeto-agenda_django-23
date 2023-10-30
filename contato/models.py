3419849f54e9111e178357b6789226bdf7a031d2
Sander Toledo <sanderaugusto@gmail.com>
2023-10-30 15:02:47 -0300

    Criando modelo user

--- contato/models.py
+++ contato/models.py
@@ -1,11 +1,16 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 class Categoria(models.Model):
     nome = models.CharField(max_length=50)
 
-    def __str__(self)->str:
+    class Meta:
+        verbose_name = "Categoria"
+        verbose_name_plural = "Categorias"
+
+    def __str__(self) -> str:
         return f"{self.nome}"
 
 
@@ -19,11 +24,9 @@ class Contato(models.Model):
     mostrar = models.BooleanField(default=False)
     imagem = models.ImageField(blank=True, upload_to="imagens/%Y/%m/")
     categoria = models.ForeignKey(
-        Categoria, 
-        on_delete=models.SET_NULL, 
-        blank=True, 
-        null=True
+        Categoria, on_delete=models.SET_NULL, blank=True, null=True
     )
+    criador = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
 
     def __str__(self) -> str:
         return f"{self.nome} {self.sobrenome}"
